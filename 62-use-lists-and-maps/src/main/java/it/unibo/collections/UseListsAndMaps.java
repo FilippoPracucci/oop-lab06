2c2c400048f27ff59faea47af5f14d3da10777db
Filippo Pracucci - filippo.pracucci@studio.unibo.it <filippo.pracucci@studio.unibo.it>
2022-10-25 17:14:34 +0200

    first implementation of 4 points of UseListsAndMaps.java

--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -1,5 +1,7 @@
 package it.unibo.collections;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -8,6 +10,11 @@ import java.util.Map;
  *
  */
 public final class UseListsAndMaps {
+
+    private static final int MIN = 1000;
+    private static final int MAX = 2000;
+    private static final int FIRST_ELEM_INDEX = 0;
+    private static final int NEW_ADD = 100_000;
 
     private UseListsAndMaps() {
     }
@@ -21,26 +28,44 @@ public final class UseListsAndMaps {
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
          */
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i <= MAX - MIN; i++) {
+            int num = i + MIN;
+            list.add(num);
+        }
+
         /*
          * 2) Create a new LinkedList<Integer> and, in a single line of code
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
+        List<Integer> newList = new LinkedList<>();
+
+        newList.addAll(list);
+        
         /*
          * 3) Using "set" and "get" and "size" methods, swap the first and last
          * element of the first list. You can not use any "magic number".
          * (Suggestion: use a temporary variable)
          */
+        Integer temp = list.get(FIRST_ELEM_INDEX);
+        list.set(FIRST_ELEM_INDEX, list.get(list.size() - 1));
+        list.set(list.size() - 1, temp);
+
         /*
          * 4) Using a single for-each, print the contents of the arraylist.
          */
+        for (Integer elem: list) {
+            System.out.println(elem);
+        }
         /*
          * 5) Measure the performance of inserting new elements in the head of
          * the collection: measure the time required to add 100.000 elements as
          * first element of the collection for both ArrayList and LinkedList,
          * using the previous lists. In order to measure times, use as example
          * TestPerformance.java.
-         */
+         */        
         /*
          * 6) Measure the performance of reading 1000 times an element whose
          * position is in the middle of the collection for both ArrayList and
